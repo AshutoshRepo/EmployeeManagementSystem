71033c57c7d16a5ef959089945e30a9d67a9d7a4
Ashutosh Singh <ashu2202.as@gmail.com>
2026-09-15 17:51:30 +0530

    Add EmployeeService class for storing employees

---
+++ src/main/java/service/EmployeeService.java
@@ -0,0 +1,13 @@
+package service;
+import java.util.List;
+import java.util.ArrayList;
+import model.Employee;
+
+public class EmployeeService {
+    private List<Employee> employees;
+    public EmployeeService()
+    {
+        employees=new ArrayList<>();
+    }
+}
+    
