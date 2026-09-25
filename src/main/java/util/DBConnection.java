3251e52a9f7dd5052e0fad40b29a0c877b3c9586
Ashutosh Singh <ashu2202.as@gmail.com>
2026-09-25 20:28:15 +0530

    Add DBConnection utility class for JDBC connectivity

---
+++ src/main/java/util/DBConnection.java
@@ -0,0 +1,15 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static String URL="jdbc:mysql://localhost:3306/ems_db";
+    private static String USERNAME="root";
+    private static String PASSWORD="Ashu@2202";
+    public static Connection getConnection() throws SQLException
+    {
+        return DriverManager.getConnection(URL,USERNAME,PASSWORD);
+    }
+}
